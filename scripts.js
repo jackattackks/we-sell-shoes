8bdb9dfeed2e5c10187170f4cac672526dc2fdcb
Jackie <Jackie@jackies-mbp.attlocal.net>
2019-05-13 15:42:41 -0700

    fifth commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,33 @@
+/////////popup//////////////
+
+//declare default vars
+const popUp = document.getElementById('pop-up');
+const popUpLayer = document.getElementById('pop-up-layer');
+const closeModal = document.getElementById('close-pop-up');
+
+//nav-bar button
+const subBtn = document.getElementById('sub-btn');
+
+//navbar button click makes the popup appear
+subBtn.addEventListener("click", () => {
+  popUpLayer.style.display = 'flex';
+})
+
+//close model click makes the popup disappear
+closeModal.addEventListener("click", function() {
+  popUpLayer.style.display = 'none';
+})
+
+let popUpSeconds = 40;
+
+//trigger popup after seconds variable
+let popUpFun = window.setInterval(function() {
+  popUpLayer.style.display = 'flex';
+
+  window.clearInterval(popUpFun);
+
+}, popUpSeconds * 1000);
+
 /*************
 testimonial section
 **************/
